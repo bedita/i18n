e3517730e9cad49fa73a707299c274ed4f5e3d55
dante di domenico <dante@didoda.com>
2022-03-28 17:01:53 +0200

    fix: getResponseWithCookie

--- src/Middleware/I18nMiddleware.php
+++ src/Middleware/I18nMiddleware.php
@@ -201,11 +201,11 @@ class I18nMiddleware implements MiddlewareInterface
      *
      * The cookie is added only if the middleware is configured to create cookie.
      *
-     * @param \Psr\Http\Message\ResponseInterface $response The response.
+     * @param \Cake\Http\Response $response The response.
      * @param string $locale The locale string to set in cookie.
      * @return \Psr\Http\Message\ResponseInterface
      */
-    protected function getResponseWithCookie(ResponseInterface $response, string $locale): ResponseInterface
+    protected function getResponseWithCookie(Response $response, string $locale): ResponseInterface
     {
         $name = $this->getConfig('cookie.name');
         $create = $this->getConfig('cookie.create', false);
